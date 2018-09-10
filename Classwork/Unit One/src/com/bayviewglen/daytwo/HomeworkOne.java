482c67b1479407fe17768ead0ed6dffc375b3209
Nicolas Tong <ntong@bayviewglen.ca>
2018-09-10 11:14:23 -0400

    Finished Homework

---
+++ Classwork/Unit One/src/com/bayviewglen/daytwo/HomeworkOne.java
@@ -0,0 +1,13 @@
+package com.bayviewglen.daytwo;
+
+public class HomeworkOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double r = 2.0;
+		double a = Math.PI * r * r;
+		System.out.println("Area is: " + a);
+	}
+
+}
---
+++ Classwork/Unit One/src/com/bayviewglen/daytwo/HomeworkThree.java
@@ -0,0 +1,13 @@
+package com.bayviewglen.daytwo;
+
+public class HomeworkThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 2, b = 17, c = 4, x = -2;
+		double y = a*Math.pow(x, 2) + b*x + c;
+		
+		System.out.println("The y value at x = " + x + " is " + y);
+	}
+
+}
